7de2bae4cc96c0795a467cf85ab7c97497b56e9f
Allen <hotlcc@qq.com>
2019-04-22 11:22:58 +0800

    [update] 1.调整了发送消息相关方法的名称;2.新增获取联系人头像数据的方法;

--- src/test/java/TestClass.java
+++ src/test/java/TestClass.java
@@ -9,6 +9,9 @@ import org.junit.Before;
 import org.junit.Test;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 public class TestClass {
     private Wechat wechat;
@@ -29,19 +32,41 @@ public class TestClass {
     }
 
     public void testSendText() {
-        JSONObject result = wechat.sendText(null, "这是消息内容");
+        JSONObject result = wechat.sendTextToUserName(null, "这是消息内容");
         System.out.println(result);
     }
 
-    @Test
     public void testSendImage() {
         File file = null;
         JSONObject result = null;
         file = new File("C:\\Users\\Administrator\\Pictures\\壁纸\\9e5f4981099bcf351e0ec18c3654aced.jpg");
-        result = wechat.sendImage(null, file);
+        result = wechat.sendImageToUserName(null, file);
         file = new File("C:\\Users\\Administrator\\Videos\\手机QQ视频_20190416170016.mp4");
-        result = wechat.sendVideo(null, file);
+        result = wechat.sendVideoToUserName(null, file);
         System.out.println(result);
         while (true) CommonUtil.threadSleep(5000);
+    }
+
+    @Test
+    public void testGetImageData() {
+        byte[] data = wechat.getContactHeadImgByUserName(null);
+        FileOutputStream fos = null;
+        try {
+            fos = new FileOutputStream("D:\\a.jpg");
+            fos.write(data);
+            fos.flush();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (fos != null) {
+                try {
+                    fos.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 }
